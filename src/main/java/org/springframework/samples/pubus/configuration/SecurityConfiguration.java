8e23bdb234a2e2cbdb6a9b9179567f593617f31d
Lucperrom <luciaperezromero07@gmail.com>
2023-12-29 14:19:12 +0100

    Conf-Configuración y estructura del proyecto

---
+++ src/main/java/org/springframework/samples/pubus/configuration/SecurityConfiguration.java
@@ -0,0 +1,54 @@
+package org.springframework.samples.pubus.configuration;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+	@Autowired
+	DataSource dataSource;
+
+    @Bean
+	protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+		
+		http
+			.cors(withDefaults())		
+			.csrf(AbstractHttpConfigurer::disable)		
+			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))			
+			.headers((headers) -> headers.frameOptions((frameOptions) -> frameOptions.disable()));		
+
+            return http.build();}      
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception{
+		return config.getAuthenticationManager();
+	}	
+
+
+}
---
+++ src/main/webapp/WEB-INF/tags/menu.tag
@@ -0,0 +1,30 @@
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="pubus" tagdir="/WEB-INF/tags"%>
+<%@ taglib prefix="sec"
+	uri="http://www.springframework.org/security/tags"%>
+<!--  >%@ taglib prefix="sec" uri="http://www.springframework.org/security/tags"%-->
+<%@ attribute name="name" required="true" rtexprvalue="true"
+	description="Name of the active menu: home, researchers, vets or error"%>
+
+<nav class="navbar navbar-default" role="navigation">
+	<div class="container">
+		<div class="navbar-header">
+			<a class="navbar-brand"
+				href="<spring:url value="/" htmlEscape="true" />"><span></span></a>
+			<button type="button" class="navbar-toggle" data-toggle="collapse"
+				data-target="#main-navbar">
+				<span class="sr-only"><os-p>Toggle navigation</os-p></span> <span
+					class="icon-bar"></span> <span class="icon-bar"></span> <span
+					class="icon-bar"></span>
+			</button>
+		</div>
+		<div class="navbar-collapse collapse" id="main-navbar">
+			<ul class="nav navbar-nav">
+
+		</div>
+
+
+
+	</div>
+</nav>
