8f97e34ade567511a8bde3cb76d8ad966ae5bba9
Lucperrom <luciaperezromero07@gmail.com>
2025-04-26 21:46:27 +0200

    conf: ampliar permisos para producción

--- src/main/java/org/springframework/samples/pubus/configuration/SecurityConfiguration.java
+++ src/main/java/org/springframework/samples/pubus/configuration/SecurityConfiguration.java
@@ -83,7 +83,7 @@ public class SecurityConfiguration {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000")); // Reemplaza con el origen de tu frontend
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://tfm-pubus.onrender.com/")); // Reemplaza con el origen de tu frontend
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
