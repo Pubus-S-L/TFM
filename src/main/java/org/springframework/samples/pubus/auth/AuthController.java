4fb2723c3f0902f610a0a60cdabe5f85b622c236
Lucperrom <luciaperezromero07@gmail.com>
2025-04-26 23:15:38 +0200

    fix: prueba de acceso

--- src/main/java/org/springframework/samples/pubus/auth/AuthController.java
+++ src/main/java/org/springframework/samples/pubus/auth/AuthController.java
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -175,7 +176,7 @@ public class AuthController {
 	}
 
 	
-	@PostMapping("/signup")	
+	@RequestMapping(value = "/signup", method = {RequestMethod.POST, RequestMethod.OPTIONS})
 	public ResponseEntity<MessageResponse> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
 		System.out.println("Signup request received: " + signUpRequest.getUsername());
 		if (userService.existsUser(signUpRequest.getUsername()).equals(true)) {
