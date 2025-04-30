6db7ae082156bbe2e877fb94d87d10cd0dd10156
Lucperrom <luciaperezromero07@gmail.com>
2025-04-30 20:50:00 +0200

    corrección de privateRoute para enrutado

--- frontend/src/privateRoute/index.js
+++ frontend/src/privateRoute/index.js
@@ -1,31 +1,55 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import tokenService from '../services/token.service';
+import { Navigate, useLocation } from 'react-router-dom';
 import Login from '../auth/login';
 
 const PrivateRoute = ({ children }) => {
-    const jwt = tokenService.getLocalAccessToken();
-    const [isLoading, setIsLoading] = useState(true);
-    const [isValid, setIsValid] = useState(null);
-    const [message, setMessage] = useState(null);
-    if (jwt) {
-        fetch(`https://tfm-m1dn.onrender.com/api/v1/auth/validate?token=${jwt}`, {
+  const jwt = tokenService.getLocalAccessToken();
+  const [isLoading, setIsLoading] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(null);
+  const [message, setMessage] = useState(null);
+  const location = useLocation();
+
+  useEffect(() => {
+    const validateToken = async () => {
+      setIsLoading(true);
+      if (jwt) {
+        try {
+          const response = await fetch(`https://tfm-m1dn.onrender.com/api/v1/auth/validate?token=${jwt}`, {
             method: 'GET',
             headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
             },
-        }).then(response => {
-            return response.json();
-        }).then(isValid => {
-            setMessage("Your token has expired. Please, sign in again.")
-            setIsValid(isValid);
-            setIsLoading(false);
-        });
-    } else return <Login message={message} navigation={false} />;
+          });
+          const data = await response.json();
+          setIsAuthenticated(data);
+          setIsLoading(false);
+        } catch (error) {
+          console.error("Error validating token:", error);
+          setMessage("There was an error validating your session. Please log in again.");
+          setIsAuthenticated(false);
+          setIsLoading(false);
+        }
+      } else {
+        setIsAuthenticated(false);
+        setIsLoading(false);
+        setMessage(null); // No hay token, no hay mensaje específico de expiración
+      }
+    };
 
-    if (isLoading === true) {
-        return <div>Loading...</div>;
-    } else return isValid === true ? children : <Login message={message} navigation={true} />
+    validateToken();
+  }, [jwt]); // Dependencia en jwt para re-ejecutar si el token cambia
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isAuthenticated === true) {
+    return children;
+  } else {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
 };
 
 export default PrivateRoute;
