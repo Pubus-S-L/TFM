d1ee74e3ed26bc37fef833fce30069ba9f992644
Lucperrom <luciaperezromero07@gmail.com>
2025-04-30 21:18:04 +0200

    fix: enrutamiento v3.0

--- frontend/src/privateRoute/index.js
+++ frontend/src/privateRoute/index.js
@@ -1,16 +1,31 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
+import React, { useState } from 'react';
 import tokenService from '../services/token.service';
+import Login from '../auth/login';
 
 const PrivateRoute = ({ children }) => {
-  const isAuthenticated = !!tokenService.getLocalAccessToken();
-  
-  // Redirige al login si no está autenticado
-  if (!isAuthenticated) {
-    return <Navigate to="/login" />;
-  }
-  
-  return children;
+    const jwt = tokenService.getLocalAccessToken();
+    const [isLoading, setIsLoading] = useState(true);
+    const [isValid, setIsValid] = useState(null);
+    const [message, setMessage] = useState(null);
+    if (jwt) {
+        fetch(`https://tfm-m1dn.onrender.com/api/v1/auth/validate?token=${jwt}`, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+        }).then(response => {
+            return response.json();
+        }).then(isValid => {
+            setMessage("Your token has expired. Please, sign in again.")
+            setIsValid(isValid);
+            setIsLoading(false);
+        });
+    } else return <Login message={message} navigation={false} />;
+
+    if (isLoading === true) {
+        return <div>Loading...</div>;
+    } else return isValid === true ? children : <Login message={message} navigation={true} />
 };
 
 export default PrivateRoute;
